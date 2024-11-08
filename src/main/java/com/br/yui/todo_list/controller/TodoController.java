9464c485fafa77d4a604cf746a5700399b667aa2
rianfernandesno <zrpapz12@gmail.com>
2024-11-08 11:38:09 -0300

    TodoController

---
+++ src/main/java/com/br/yui/todo_list/controller/TodoController.java
@@ -0,0 +1,34 @@
+package com.br.yui.todo_list.controller;
+
+import com.br.yui.todo_list.entity.Todo;
+import com.br.yui.todo_list.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    private TodoController(TodoService todoService){
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+    @GetMapping
+    public List<Todo> list(){
+        return todoService.list();
+    }
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable Long id){
+        return todoService.delete(id);
+    }
+}
