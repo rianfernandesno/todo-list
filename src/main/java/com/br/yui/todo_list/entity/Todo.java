18e0bb5d1acc53f35c7b0e226c1be02fd84d42fd
rianfernandesno <zrpapz12@gmail.com>
2024-11-08 11:26:00 -0300

    Todo Entity

---
+++ src/main/java/com/br/yui/todo_list/entity/Todo.java
@@ -0,0 +1,55 @@
+package com.br.yui.todo_list.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name =  "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private Integer prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public Integer getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Integer prioridade) {
+        this.prioridade = prioridade;
+    }
+}
